ae785c641b93d44eb4a894aebd2b4be59d2a8a39
Redwan002117 <22303307@iubat.edu>
2026-01-27 08:09:54 +0600

    Add update scripts and configure docker-compose for remote image

---
+++ update_app.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Updating Repair Base App...
+
+echo 1. Pulling latest image from GitHub Container Registry...
+docker-compose pull
+
+echo 2. Restarting container...
+docker-compose up -d --remove-orphans
+
+echo 3. Cleaning up old images...
+docker image prune -f
+
+echo Update Complete! Access at http://localhost:8080
+pause
